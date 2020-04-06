1d1adf82934eb127072c583ac3fd2ac1cab5a318
ghost_cfl <946930866@qq.com>
2020-04-06 18:10:44 +0800

    修改两个方法的名

--- tools/tools.py
+++ tools/tools.py
@@ -2,12 +2,12 @@ import time
 import datetime
 
 
-def _time_format(format_string):
+def time_format(format_string):
     return datetime.datetime.now().strftime(format_string)
 
 
-def _sleep(seconds=60):
-    print(_time_format("%Y-%d-%m %H:%M:%S") + " | ", end="", flush=True)
+def my_sleep(seconds=60):
+    print(time_format("%Y-%d-%m %H:%M:%S") + " | ", end="", flush=True)
     for _ in range(int(seconds)):
         time.sleep(1)
         print(">", end="", flush=True)
