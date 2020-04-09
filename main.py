26c112dd04cbd3ea0484a84173aa3674befe16fe
ghost_cfl <946930866@qq.com>
2020-04-09 18:13:33 +0800

    尝试异步运行

--- main.py
+++ main.py
@@ -6,18 +6,36 @@ from core.spiders.order_detail_link_id_spider import OrderDetailLinkIDSpider
 from core.browser.login_tb import LoginTB
 from settings import STORE_INFO
 
+
+async def task_1(o_l_p_s, o_d_p_s, o_d_l_id_s):
+    page_num = 1
+    while 1:
+        completed = await o_l_p_s.get_page(page_num)
+        if completed == 1:
+            page_num += 1
+        elif completed == 2:
+            page_num = 1
+        await o_d_p_s.get_page(),
+        await o_d_l_id_s.run()
+
+
+async def task_2(o_d_p_s, o_d_l_id_s):
+    while 1:
+        await asyncio.sleep(30)
+        await o_d_p_s.get_page(),
+        await o_d_l_id_s.run()
+
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     l, b, p, f = loop.run_until_complete(LoginTB.run(**STORE_INFO['KY']))
     o_l_p_s = OrderListPageSpider(l, b, p, f)
     o_d_p_s = OrderDetailPageSpider(l, b, p, f)
     o_d_l_id_s = OrderDetailLinkIDSpider(l, b, p, f)
-    page_num = 1
-    while 1:
-        completed = loop.run_until_complete(o_l_p_s.get_page(page_num))
-        if completed == 1:
-            page_num += 1
-        elif completed == 2:
-            page_num = 1
-        loop.run_until_complete(o_d_p_s.get_page())
-        # input("阻塞进程用的，后续要删除")
+
+    tasks = [
+        task_1(o_l_p_s, o_d_p_s, o_d_l_id_s),
+        # task_2(o_d_p_s, o_d_l_id_s)
+    ]
+    loop.run_until_complete(asyncio.wait(tasks))
+    # input("阻塞进程用的，后续要删除")
