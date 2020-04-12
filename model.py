b7aff524f7a9533a16bb18f0bd8c8f1fefce076c
ghostcfl <946930866@qq.com>
2020-04-13 00:38:42 +0800

    添加price_erp默认值 为0

--- model.py
+++ model.py
@@ -115,6 +115,7 @@ class PriceTBItem(BaseItem):
         self.shop_id = kwargs.get('shop_id')
         self.typeabbrev = kwargs.get('typeabbrev')
         self.price_tb = kwargs.get('price_tb')
+        self.price_erp = kwargs.get('price_erp', 0)
         self.currabrev = kwargs.get('currabrev', "CNY")
         self.operator = kwargs.get('operator', '爬虫维护')
         self.last_time = kwargs.get('last_time')
