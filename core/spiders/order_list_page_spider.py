3710e06123f8ff308f8bd989566ab3a3cfd618f6
ghost_cfl <946930866@qq.com>
2020-04-14 13:25:09 +0800

    修改了爬虫方式，浏览器获取cookie后，使用request请求，cookie过期后再重新获取

--- core/spiders/order_list_page_spider.py
+++ core/spiders/order_list_page_spider.py
@@ -16,46 +16,123 @@ from db.my_sql import MySql
 class OrderListPageSpider(BaseSpider):
     base_url = "https://trade.taobao.com"
     url = 'https://trade.taobao.com/trade/itemlist/asyncSold.htm?event_submit_do_query=1&_input_charset=utf8'
+    data = {
+        'auctionType': '0',
+        'close': '0',
+        'pageNum': '4',
+        'pageSize': '15',
+        'queryMore': 'true',
+        'rxAuditFlag': '0',
+        'rxElectronicAllFlag': '0',
+        'rxElectronicAuditFlag': '0',
+        'rxHasSendFlag': '0',
+        'rxOldFlag': '0',
+        'rxSendFlag': '0',
+        'rxSuccessflag': '0',
+        'rxWaitSendflag': '0',
+        'tradeTag': '0',
+        'useCheckcode': 'false',
+        'useOrderInfo': 'false',
+        'errorCheckcode': 'false',
+        'action': 'itemlist/SoldQueryAction',
+        'prePageNo': '3',
+        'buyerNick': '',
+        'dateBegin': '0',
+        'dateEnd': '0',
+        'logisticsService': '',
+        'orderStatus': '',
+        'queryOrder': 'desc',
+        'rateStatus': '',
+        'refund': '',
+        'sellerNick': '',
+        'tabCode': 'latest3Months'
+    }
 
     async def intercept_response(self, res):
         req = res.request
         if res.url == self.url:
             a = await res.json()
-            try:
-                await self.parse(a['mainOrders'], a['page']['currentPage'])
+            if a.get("mainOrders"):
+                logger.info("重置cookies成功")
                 write(flag="headers", value=req.headers)
-            except KeyError:
-                logger.error("KeyError")
+            else:
+                pass
+            # try:
+            #     await self.parse(a['mainOrders'], a['page']['currentPage'])
+            #     write(flag="headers", value=req.headers)
+            # except KeyError:
+            #     logger.error("KeyError")
+
+    async def set_post_headers(self):
+        delete("headers")
+        while 1:
+            frames = self.page.frames
+            frame = await self.login.get_nc_frame(frames)
+            if not frame:
+                headers = read(flag="headers")
+                if not headers:
+                    await self.listening(self.page)
+                    try:
+                        await self.page.waitForSelector(".pagination-item-1.pagination-item-active", timeout=3000)
+                    except Exception as e:
+                        await self.page.click(".pagination-item-1")
+                    else:
+                        await self.page.click(".pagination-item-2")
+                else:
+                    break
+            else:
+                await self.login.slider(self.page)
+            await asyncio.sleep(5)
+        # print(headers)
 
     async def get_page(self, page_num):
-        self.completed = 0
-        try:
-            await self.page.waitForSelector(".pagination-options-go")
-            await self.page.focus(".pagination-options input")
-            for _ in range(3):
-                await self.page.keyboard.press("Delete")
-                await self.page.keyboard.press("Backspace")
-            await self.listening(self.page)
-            await self.page.type(".pagination-options input", str(page_num))
-            await self.page.keyboard.press("Enter")
-            restart = await self.login.slider(self.page)
-            if restart:
-                exit("滑块验证码失败，退出")
-            self.page.waitForSelector(
-                ".pagination-item.pagination-item-" + str(page_num) + ".pagination-item-active",
-                timeout=10000)
-        except Exception as e:
-            if re.search('\"\.pagination-options-go\"', str(e)):
-                restart = await self.login.slider(self.page)
-                if restart:
-                    exit("滑块验证码失败，退出")
-            else:
-                logger.error(str(e))
+        self.data['pageNum'] = page_num
         while 1:
-            if self.completed:
-                await my_async_sleep(10, True)
-                return self.completed
-            await asyncio.sleep(2)
+            headers = read("headers")
+            if headers:
+                logger.info("开始订单列表第 " + str(page_num) + " 页爬取")
+                logger.info(store_trans(self.fromStore))
+                r = requests.post(self.url, data=self.data, headers=headers)
+                a = r.json()
+                if a.get("mainOrders"):
+                    await self.parse(a['mainOrders'], a['page']['currentPage'])
+                    logger.info("完成订单列表第 " + str(page_num) + " 页订单爬取")
+                    return self.completed
+                else:
+                    logger.info("headers失效，重置cookies")
+                    await self.set_post_headers()
+            else:
+                logger.info("headers失效，重置cookies")
+                await self.set_post_headers()
+            await asyncio.sleep(10)
+        # self.completed = 0
+        # try:
+        #     await self.page.waitForSelector(".pagination-options-go")
+        #     await self.page.focus(".pagination-options input")
+        #     for _ in range(3):
+        #         await self.page.keyboard.press("Delete")
+        #         await self.page.keyboard.press("Backspace")
+        #     await self.listening(self.page)
+        #     await self.page.type(".pagination-options input", str(page_num))
+        #     await self.page.keyboard.press("Enter")
+        #     restart = await self.login.slider(self.page)
+        #     if restart:
+        #         exit("滑块验证码失败，退出")
+        #     self.page.waitForSelector(
+        #         ".pagination-item.pagination-item-" + str(page_num) + ".pagination-item-active",
+        #         timeout=10000)
+        # except Exception as e:
+        #     if re.search('\"\.pagination-options-go\"', str(e)):
+        #         restart = await self.login.slider(self.page)
+        #         if restart:
+        #             exit("滑块验证码失败，退出")
+        #     else:
+        #         logger.error(str(e))
+        # while 1:
+        #     if self.completed:
+        #         await my_async_sleep(10, True)
+        #         return self.completed
+        #     await asyncio.sleep(2)
 
     async def parse(self, main_orders, page_num):
         # print(main_orders)
@@ -69,11 +146,7 @@ class OrderListPageSpider(BaseSpider):
         else:
             eoc = 60
 
-        start_time = datetime.datetime.now()
-        logger.info("开始第 " + str(page_num) + " 页订单爬取")
-        logger.info(store_trans(self.fromStore))
         for i in range(len(main_orders)):
-            loop_control = 0  # 退出循环的控制变量
             continue_code = 0  # 有些订单的商品,在未付款时就已经退掉了,所以直接直接将数据进行删除
             # 解析并保存订单到数据库
             sub_orders, tb_order_item = await self.parse_order_item(i, main_orders, ms)
@@ -84,11 +157,6 @@ class OrderListPageSpider(BaseSpider):
             date_limit = (date - datetime.timedelta(eoc)).strftime("%Y-%m-%d %H:%M:%S")
             if tb_order_item.createTime < date_limit:
                 logger.info("完成本轮爬取，共翻 " + str(page_num) + " 页。")
-
-                ms.update(t="tb_order_spider", set={"isDetaildown": 0},
-                          c={"isDetaildown": 2, "fromStore": self.fromStore})
-                ms.update(t="tb_order_spider", set={"isVerify": 0},
-                          c={"isVerify": 2, "fromStore": self.fromStore})
                 self.completed = 2
                 del ms
                 return
@@ -188,7 +256,12 @@ class OrderListPageSpider(BaseSpider):
             if completed == 1:
                 page_num += 1
             elif completed == 2:
+                MySql.cls_update(t="tb_order_spider", set={"isDetaildown": 0},
+                                 c={"isDetaildown": 2, "fromStore": from_store})
+                MySql.cls_update(t="tb_order_spider", set={"isVerify": 0},
+                                 c={"isVerify": 2, "fromStore": from_store})
                 page_num = 1
+            await my_async_sleep(15, random_sleep=True)
 
 
 class DelayOrderUpdate(OrderListPageSpider):
@@ -254,64 +327,72 @@ class DelayOrderUpdate(OrderListPageSpider):
     }
 
     async def get_page(self, page_num=None):
-        delete("headers")
+        while 1:
+            headers = read("headers")
+            if headers:
+                days, order_no = self._get_order_info()
+                if days > 90:
+                    data = self.data_before_3_month.copy()
+                else:
+                    data = self.data.copy()
+                if order_no:
+                    logger.info("滞留订单 " + order_no + " 开始爬取")
+                    data['orderId'] = order_no
+                    r = requests.post(self.url, data=data, headers=headers)
+                    a = r.json()
+                    if a.get('mainOrders'):
+                        await self.parse(a['mainOrders'], a['page']['currentPage'])
+                        logger.info("滞留订单 " + order_no + " 爬取完成")
+                        return self.completed
+                    else:
+                        logger.info("headers失效,需要重重置cookies")
+                        delete("headers")
+                else:
+                    break
+            await asyncio.sleep(15)
+
+    def _get_order_info(self):
         today = datetime.datetime.now()
         one_day = datetime.timedelta(minutes=60)
         earlier_15_minutes = today - one_day
         updateTime = earlier_15_minutes.strftime("%Y-%m-%d %H:%M:%S")
         payTime = yesterday("18:00:00")
         sql = """      
-               SELECT 
-               tos.orderNo,createTime
-               FROM tb_order_spider tos
-               WHERE  tos.updateTime<'{}'
-               AND tos.`orderStatus` = '买家已付款' 
-               AND tos.`fromStore` = '{}' 
-               AND tos.payTime<'{}'
-               ORDER BY updateTime;
-               """.format(updateTime, self.fromStore, payTime)
-        headers = read("headers")
+                               SELECT 
+                               tos.orderNo,createTime
+                               FROM tb_order_spider tos
+                               WHERE  tos.updateTime<'{}'
+                               AND tos.`orderStatus` = '买家已付款' 
+                               AND tos.`fromStore` = '{}' 
+                               AND tos.payTime<'{}'
+                               ORDER BY updateTime;
+                               """.format(updateTime, self.fromStore, payTime)
         res = MySql.cls_get_dict(sql=sql)
-        order_no = res[0]['orderNo']
-        days = (today - res[0]['createTime']).days
-        # print(days)
-        if days > 90:
-            data = self.data_before_3_month.copy()
-        else:
-            data = self.data.copy()
-        if headers and order_no:
-            logger.info(order_no)
-            data['orderId'] = order_no
-            r = requests.post(self.url, data=data, headers=headers)
-            a = r.json()
-            # print(a)
-            try:
-                await self.parse(a['mainOrders'], a['page']['currentPage'])
-            except KeyError:
-                delete("headers")
-                logger.error("KeyError")
-            else:
-                pass
-            # if self.completed:
+        order_no = None
+        days = 0
+        if res:
+            order_no = res[0]['orderNo']
+            days = (today - res[0]['createTime']).days
+        return days, order_no
 
     @classmethod
     async def run(cls, login, browser, page, from_store):
         while 1:
             delay_order_spider = DelayOrderUpdate(login, browser, page, from_store)
             await delay_order_spider.get_page()
-            await my_async_sleep(15)
+            await my_async_sleep(15, random_sleep=True)
 
 
 if __name__ == '__main__':
     from core.browser.login_tb import LoginTB
     from settings import STORE_INFO
 
+    delete("headers")
     loop = asyncio.get_event_loop()
     l, b, p, f = loop.run_until_complete(LoginTB.run(**STORE_INFO['KY']))
-
     dou = DelayOrderUpdate(l, b, p, f)
     tasks = [
-        # OrderListPageSpider.run(l, b, p, f),
-        dou.get_page()
+        OrderListPageSpider.run(l, b, p, f),
+        DelayOrderUpdate.run(l, b, p, f)
     ]
-    loop.run_until_complete(dou.get_page())
+    loop.run_until_complete(asyncio.wait(tasks))
